05559fa209b62d7e5e38c71cb81ad23682bd0001
ΛsﾠΛɴᴀɴᴛﾠ⸙ <anantisback47@gmail.com>
2025-10-26 22:00:10 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,10 +4,11 @@ buttons.forEach(btn => {
   btn.addEventListener("click", () => {
     const productName = btn.parentElement.querySelector("h2").innerText;
 
-    fetch("https://YOUR_RENDER_BACKEND_URL/add-to-cart", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ product: productName })
+    fetch("https://veloxyra-store.onrender.com/add-to-cart", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ product: productName })
+})
     })
     .then(res => res.json())
     .then(data => {
